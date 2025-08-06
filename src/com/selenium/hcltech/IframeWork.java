65659f16c8222f52c18690cfc0e83c0c224f2c0c
Inturi Dhineesh <inturi.dhineesh@hcltech.com>
2025-08-06 17:26:19 +0530

    updated the iframe file

--- src/com/selenium/hcltech/IframeWork.java
+++ src/com/selenium/hcltech/IframeWork.java
@@ -15,13 +15,14 @@ public class IframeWork {
 		driver.manage().window().maximize();
 		driver.get("https://the-internet.herokuapp.com/");
 		Thread.sleep(3000);
+		System.out.println("there is 3 second time delay");
 		//driver.findElement(By.id("")).sendKeys("divya");
 		driver.findElement(By.xpath("//*[@id=\"content\"]/ul/li[22]/a")).click();
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("//*[@id=\"content\"]/div/ul/li[2]/a")).click();
 		Thread.sleep(3000);
 		WebElement alertBox = driver.findElement(By.xpath("/html/body/div[4]/div/div"));
-		System.out.println(alertBox.getText());
+		System.out.println(alertBox.getText()+"alert box is working properly");
 		driver.switchTo().frame("mce_0_ifr");
 		driver.switchTo().defaultContent();
 
